b3b753c28db878fd8f14b612643c837f6b7f0ee2
Alberto Alonso <rydencillo@gmail.com>
2009-04-28 16:02:50 +0000

    Proyecto Núcleo.

---
+++ Nucleo/Nucleo.cpp
@@ -0,0 +1,23 @@
+/////////////////////////////////////////////////////////////
+//
+// Servicios de redhispana.org
+// Está prohibida la reproducción y el uso parcial o total
+// del código fuente de estos servicios sin previa
+// autorización escrita del autor de estos servicios.
+//
+// Si usted viola esta licencia se emprenderán acciones legales.
+//
+// (C) RedHispana.Org 2009
+//
+// Archivo:     Nucleo.cpp
+// Propósito:   Núcleo del programa. Inicialización y bucle.
+// Autores:     Alberto Alonso <rydencillo@gmail.com>
+//
+
+#include "stdafx.h"
+
+int main( int argc, const char* argv[], const char* envp[] )
+{
+    return 0;
+}
+
---
+++ Nucleo/stdafx.h
@@ -0,0 +1,20 @@
+/////////////////////////////////////////////////////////////
+//
+// Servicios de redhispana.org
+// Está prohibida la reproducción y el uso parcial o total
+// del código fuente de estos servicios sin previa
+// autorización escrita del autor de estos servicios.
+//
+// Si usted viola esta licencia se emprenderán acciones legales.
+//
+// (C) RedHispana.Org 2009
+//
+// Archivo:     stdafx.h
+// Propósito:   Encabezado precompilado.
+// Autores:     Alberto Alonso <rydencillo@gmail.com>
+//
+
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
